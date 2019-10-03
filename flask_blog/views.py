cf8ef0292437ffa225d2eb7c6f9e143dae347b9c
nomujun <nomujun@nomujunnoMacBook-Pro.local>
2019-10-04 08:22:31 +0900

    first commit

---
+++ flask_blog/views.py
@@ -0,0 +1,32 @@
+from flask_blog import app
+from flask import request,redirect,url_for,render_template,flash,session
+
+
+
+@app.route('/')
+def show_entries():
+    if not session.get('logged_in'):
+        return redirect('/login')
+    return render_template('entries/index.html')
+
+
+# ログイン
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            flash('ユーザー名が異なります')
+        elif request.form['password'] != app.config['PASSWORD'] != app.config['PASSWORD']:
+            flash('パスワードが異なります')
+        else:
+            flash('ログインしました')
+            session['logged_in'] = True
+            return redirect('/')
+    
+    return render_template('login.html')
+# ログアウト
+@app.route('/logout')
+def logout():
+    session.pop('logged_in',None)
+    flash('ログアウトしました')
+    return redirect('/')
